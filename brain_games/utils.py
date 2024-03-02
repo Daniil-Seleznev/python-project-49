bac639827631ea3f0e2c77480baf3eb9a63087a9
Daniil-Seleznev <seleznve.d.ole@gmail.com>
2024-03-03 00:14:46 +0400

    refactor utils

--- brain_games/utils.py
+++ brain_games/utils.py
@@ -2,7 +2,7 @@ import prompt
 from random import randint
 
 
-def answer_try(right_answer):
+def answer_try(right_answer: str) -> bool:
     user_answer = prompt.string('Your answer: ')
     if user_answer == right_answer:
         print('Correct!')
@@ -13,11 +13,11 @@ def answer_try(right_answer):
         return False
 
 
-def random_number():
+def random_number() -> int
     return randint(1, 100)
 
 
-def get_answer_of_expression(number1, number2, operation):
+def get_answer_of_expression(number1: int, number2: int, operation: str) -> str:
     if operation == '+':
         return str(number1 + number2)
     elif operation == '-':
@@ -26,11 +26,11 @@ def get_answer_of_expression(number1, number2, operation):
         return str(number1 * number2)
 
 
-def is_even(number):
+def is_even(number: int) -> bool:
     return number % 2 == 0
 
 
-def get_gcd(a, b):
+def get_gcd(a: int, b: int) -> str:
     while b:
         a, b = b, a % b
     return str(a)
